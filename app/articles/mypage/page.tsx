e1366f54e8846c1d4b6790d88a40a84c24bacdce
Boyd <boyd@emocog.com>
2025-01-08 17:53:31 +0900

    feat: mypage

--- app/articles/mypage/page.tsx
+++ app/articles/mypage/page.tsx
@@ -1,7 +1,24 @@
-import {Header} from "@/components/header";
+'use client';
 
-export default function MyPage() {
-  return <div>
-    <Header/>
-  </div>;
+import { Header } from '@/components/header';
+import { useRouter } from 'next/navigation';
+import { PATH } from '@/lib/path';
+import './_style.css';
+import HomeButton from '@/components/homeButton';
+
+export default MyPage;
+
+function MyPage() {
+  const router = useRouter();
+  return (
+    <div className="mypage-container">
+      <Header />
+      <div className="button-container">
+        <button onClick={() => router.replace(PATH.SIGNIN)}>LOGIN</button>
+        <button onClick={() => router.replace(PATH.SIGNUP)}>JOIN</button>
+        <button onClick={() => router.replace(PATH.TEST)}>AUTH TEST</button>
+      </div>
+      <HomeButton />
+    </div>
+  );
 }
